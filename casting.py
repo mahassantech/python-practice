d87883ec5d6bf847c335c3f84ad90e2eabb5a981
Hassan <mahassantech@gmail.com>
2026-03-02 23:27:25 +0600

    type casting

---
+++ casting.py
@@ -0,0 +1,31 @@
+# integers practice 
+a = int(1)   
+b = int(2.8) 
+c = int("3") 
+
+print(a)
+print(b)
+print(c)
+
+
+# float practice 
+
+x = float(1)     
+y = float(2.8)   
+z = float("3")   
+w = float("4.2")
+
+print(x)
+print(y)
+print(z)
+print(w)
+
+# Strings practice 
+
+g = str("s1")
+h = str(2)    
+j = str(3.0) 
+
+print(g)
+print(h)
+print(j)
