d6751fc92d82db94b0072ca0bc9f582c377a5b4a
Hassan <mahassantech@gmail.com>
2026-03-21 23:21:23 +0600

    problem solving

---
+++ mxx.py
@@ -0,0 +1,8 @@
+numbers = [10, 45, 23, 89, 67]  
+
+max=numbers[0] 
+
+for num in numbers: 
+    if num>max: 
+        max=num 
+print(max)
--- s_l.py
+++ s_l.py
@@ -1,1 +1,13 @@
-numbers = [10, 45, 23, 89, 67]
+numbers = [10, 45, 23, 89, 67] 
+
+max_number=numbers[0]
+secmx_number=numbers[0]
+
+for num in numbers: 
+    if num > max_number :
+        secmx_number=max_number
+        max_number=num 
+    else:
+        if num > secmx_number and num != max_number:
+            secmx_number=num 
+print(secmx_number)
